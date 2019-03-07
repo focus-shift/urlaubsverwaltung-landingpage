a653f8912350497de209db5663d3d8c8f4b0b276
Benjamin Seber <seber@synyx.de>
2019-03-07 21:06:49 +0100

    code format

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,10 +9,9 @@ export default function IndexPage() {
 				<h1>Probier's mal mit Urlaub!</h1>
 				<p className="container mx-auto">
 					Direkt vom Strand aus den nächsten Urlaub beantragen und die Sonne
-					weiter genießen? Mit dem Urlaubär kein Problem!
-					Urlaubsplanung leicht gemacht - übersichtlich, bequem und überall
-					nutzbar. So macht nicht nur der Urlaub Spaß, sondern auch die
-					Urlaubsplanung. Echt bärenstark!
+					weiter genießen? Mit dem Urlaubär kein Problem! Urlaubsplanung leicht
+					gemacht - übersichtlich, bequem und überall nutzbar. So macht nicht
+					nur der Urlaub Spaß, sondern auch die Urlaubsplanung. Echt bärenstark!
 				</p>
 				<article>
 					<h2>Warum Urlaubär?</h2>
