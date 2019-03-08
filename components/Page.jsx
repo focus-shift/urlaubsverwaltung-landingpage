919f4efc379192286ea84edcd15e3cd8c5d80b68
Benjamin Seber <seber@synyx.de>
2019-03-08 14:38:16 +0100

    print page title with separator only when there is a page title

--- components/Page.jsx
+++ components/Page.jsx
@@ -5,10 +5,11 @@ import Header from "./Header";
 import "./style.tailwind.css";
 
 export default function Page({ title, children }) {
+	const pageTitle = ["Urlaubsverwaltung", title].filter(Boolean).join(" · ");
 	return (
 		<>
 			<Head>
-				<title>Urlaubsverwaltung · {title}</title>
+				<title>{pageTitle}</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
 			<Header />
