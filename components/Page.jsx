7dbd989bc933242527849324e40538c13cee1a59
Benjamin Seber <seber@synyx.de>
2019-03-08 14:37:46 +0100

    define <main> element in Page component

--- components/Page.jsx
+++ components/Page.jsx
@@ -12,7 +12,7 @@ export default function Page({ title, children }) {
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
 			<Header />
-			{children}
+			<main className="container mx-auto">{children}</main>
 		</>
 	);
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,21 +5,19 @@ import Benefits from "../components/Benefits";
 export default function IndexPage() {
 	return (
 		<Page>
-			<div className="container mx-auto">
-				<h1>Probier's mal mit Urlaub!</h1>
-				<p className="container mx-auto">
-					Direkt vom Strand aus den nächsten Urlaub beantragen und die Sonne
-					weiter genießen? Mit dem Urlaubär kein Problem! Urlaubsplanung leicht
-					gemacht - übersichtlich, bequem und überall nutzbar. So macht nicht
-					nur der Urlaub Spaß, sondern auch die Urlaubsplanung. Echt bärenstark!
-				</p>
-				<article>
-					<h2>Warum Urlaubär?</h2>
-					<p>Beim Urlaubär bist du in besten Pfoten!</p>
-					<Benefits />
-					<Customers />
-				</article>
-			</div>
+			<h1>Probier's mal mit Urlaub!</h1>
+			<p>
+				Direkt vom Strand aus den nächsten Urlaub beantragen und die Sonne
+				weiter genießen? Mit dem Urlaubär kein Problem! Urlaubsplanung leicht
+				gemacht - übersichtlich, bequem und überall nutzbar. So macht nicht nur
+				der Urlaub Spaß, sondern auch die Urlaubsplanung. Echt bärenstark!
+			</p>
+			<article>
+				<h2>Warum Urlaubär?</h2>
+				<p>Beim Urlaubär bist du in besten Pfoten!</p>
+				<Benefits />
+				<Customers />
+			</article>
 		</Page>
 	);
 }
