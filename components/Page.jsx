71ca7d20bab98b08195b54048f4d0b05a8356803
Benjamin Seber <seber@synyx.de>
2019-03-09 16:03:56 +0100

    add easteregg

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 
 import Header from "./Header";
@@ -7,6 +7,18 @@ import "./style.tailwind.css";
 
 export default function Page({ title, children }) {
 	const pageTitle = ["Urlaubsverwaltung", title].filter(Boolean).join(" · ");
+
+	// prettier-ignore
+	useEffect(() => {
+		console.log("%c                 ", "font-size: 48px; background-color: #b4d455");
+		console.log("%c  ʕ •ᴥ•ʔゝ☆     ", "font-size:52px; color: black; background-color: #b4d455");
+		console.log("%c                                                                          ", "font-size:11px; color: black; background-color: #b4d455");
+		console.log("%c  %c  Code With Attitude   %c         ", "background-color: #b4d455; font-size: 24px", "font-size:24px; color: #efefef; background-color: black", "background-color: #b4d455; font-size: 24px");
+		console.log("%c                                                                          ", "font-size:11px; color: black; background-color: #b4d455");
+		console.log("%c     %c  jobs@synyx.de       %c       ", "background-color: #b4d455; font-size: 24px", "font-size:24px; color: #efefef; background-color: black", "background-color: #b4d455; font-size: 24px");
+		console.log("%c                 ", "font-size: 48px; background-color: #b4d455");
+	});
+
 	return (
 		<div className="min-h-screen flex flex-col">
 			<Head>
