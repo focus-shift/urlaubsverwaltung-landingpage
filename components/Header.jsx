eb03d77de90c4054e23c6fe000fe35ba824a5c8a
Benjamin Seber <seber@synyx.de>
2019-03-09 11:59:54 +0100

    add background image and style header and frontpage stage

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,11 @@ import Navigation from "./Navigation";
 import Link from "./Link";
 import SocialMedia from "./SocialMedia";
 import bearImage from "./bear.png";
+import "./header.tailwind.css";
 
 export default function Header() {
 	return (
-		<header className="flex flex-row items-center h-10 overflow-hidden p-2 container mx-auto">
+		<header className="header flex flex-row items-center h-10 overflow-hidden p-2 container mx-auto">
 			<div className="h-full">
 				<Link href="/" className="block h-full flex items-center">
 					<img
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import Page from "../components/Page";
 import Benefits from "../components/Benefits";
 
+import "./index.tailwind.css";
+
 export default function IndexPage() {
 	return (
 		<Page>
-			<h1>Probier's mal mit Urlaub!</h1>
-			<p>
-				Direkt vom Strand aus den nächsten Urlaub beantragen und die Sonne
-				weiter genießen? Mit dem Urlaubär kein Problem! Urlaubsplanung leicht
-				gemacht - übersichtlich, bequem und überall nutzbar. So macht nicht nur
-				der Urlaub Spaß, sondern auch die Urlaubsplanung. Echt bärenstark!
-			</p>
+			<Stage />
 			<article>
 				<h2>Warum Urlaubär?</h2>
 				<p>Beim Urlaubär bist du in besten Pfoten!</p>
@@ -19,6 +15,22 @@ export default function IndexPage() {
 				<Customers />
 			</article>
 		</Page>
+	);
+}
+
+function Stage() {
+	return (
+		<div className="frontpage-stage flex justify-center">
+			<div className="md:w-5/6 p-8 sm:p-12 md:p-16 lg:p-20">
+				<h1 className="mb-4">Probier's mal mit Urlaub!</h1>
+				<p>
+					Direkt vom Strand aus den nächsten Urlaub beantragen und die Sonne
+					weiter genießen? Mit dem Urlaubär kein Problem! Urlaubsplanung leicht
+					gemacht - übersichtlich, bequem und überall nutzbar. So macht nicht
+					nur der Urlaub Spaß, sondern auch die Urlaubsplanung. Echt bärenstark!
+				</p>
+			</div>
+		</div>
 	);
 }
 
