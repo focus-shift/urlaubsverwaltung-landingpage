d10fd86246b1d4b6ce1e0caab02e852c2bc0fb1e
Aljona Buchloh <buchloh@synyx.de>
2019-04-17 11:16:52 +0200

    Remove unused pages and index page elements for now

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -7,21 +7,9 @@ export default function Navigation() {
 		<nav className="flex-1">
 			<h3 className="visually-hidden">Navigation</h3>
 			<ul className="list-reset flex justify-end">
-				<li className="p-1">
-					<Link href="/features">Features</Link>
-				</li>
-				<li className="p-1">
-					<Link href="/demo">Demo</Link>
-				</li>
-				<li className="p-1">
-					<Link href="/faq">Faq</Link>
-				</li>
-				<li className="p-1">
-					<Link href="/newsletter">Newsletter</Link>
-				</li>
-				<li className="p-1">
-					<Link href="/contact">Kontakt</Link>
-				</li>
+				{/*<li className="p-1">
+					<Link href="/impressum">Impressum</Link>
+				</li>*/}
 			</ul>
 		</nav>
 	);
