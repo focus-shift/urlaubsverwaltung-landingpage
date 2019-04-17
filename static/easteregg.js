8ce60829d93b3c45226fb0f71aa9d4c570ad8cbb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-17 13:55:12 +0000

    Deploy Urlaubsverwaltung to github.com/synyx/urlaubsverwaltung-landingpage.git:gh-pages

---
+++ static/easteregg.js
@@ -0,0 +1,32 @@
+console.log(
+	"%c                 ",
+	"font-size: 48px; background-color: #b4d455",
+);
+console.log(
+	"%c  ʕ •ᴥ•ʔゝ☆     ",
+	"font-size:52px; color: black; background-color: #b4d455",
+);
+console.log(
+	"%c                                                                          ",
+	"font-size:11px; color: black; background-color: #b4d455",
+);
+console.log(
+	"%c  %c  Code With Attitude   %c         ",
+	"background-color: #b4d455; font-size: 24px",
+	"font-size:24px; color: #efefef; background-color: black",
+	"background-color: #b4d455; font-size: 24px",
+);
+console.log(
+	"%c                                                                          ",
+	"font-size:11px; color: black; background-color: #b4d455",
+);
+console.log(
+	"%c     %c  jobs@synyx.de       %c       ",
+	"background-color: #b4d455; font-size: 24px",
+	"font-size:24px; color: #efefef; background-color: black",
+	"background-color: #b4d455; font-size: 24px",
+);
+console.log(
+	"%c                 ",
+	"font-size: 48px; background-color: #b4d455",
+);
