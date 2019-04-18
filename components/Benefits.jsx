93d5a18e89ba94aa09299b435c0db36fe59cdeb4
Benjamin Seber <seber@synyx.de>
2019-04-18 16:16:42 +0200

    fix background circle of benefit icon svg
    'display: none' results in not visible circle background

--- components/Benefits.jsx
+++ components/Benefits.jsx
@@ -58,9 +58,6 @@ const BenefitSvg = (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		xmlnsXlink="http://www.w3.org/1999/xlink"
-		viewBox="0 0 425.9 450.3"
-		width="128"
-		height="128"
 		className="benefit-image mx-auto"
 	>
 		<use xlinkHref="#benefit-icon-svg" />
@@ -68,10 +65,25 @@ const BenefitSvg = (
 );
 
 const BenefitSvgDefinition = (
-	<svg style={{ display: "none" }}>
-		<defs>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		style={{
+			display: "block",
+			width: "1px",
+			height: "1px",
+			position: "absolute",
+			top: "-99999px",
+			left: "-99999px",
+		}}
+	>
+		<symbol
+			viewBox="0 0 425.9 450.3"
+			width="128px"
+			height="128px"
+			id="benefit-icon-svg"
+		>
 			<linearGradient
-				id="benefit-icon-svg-gradient"
+				id="a"
 				x1="213"
 				x2="213"
 				y1="450.3"
@@ -81,48 +93,43 @@ const BenefitSvgDefinition = (
 				<stop offset="0" stopColor="#83ccd6" />
 				<stop offset="1" stopColor="#c8e2c9" />
 			</linearGradient>
-		</defs>
-		<defs>
-			<g id="benefit-icon-svg">
-				<circle
-					cx="213"
-					cy="237.3"
-					r="213"
-					fill="url(#benefit-icon-svg-gradient)"
-				/>
-				<path
-					fill="#7e5635"
-					d="M395.1 224.7c-7-22.4-13.5-42.5-19.2-59.3-17-50.7-34.3-149-162.3-149-130.8 0-145 98.3-162.2 149-5.7 16.8-12.3 36.9-19.4 59.3l-23 74a213 213 0 0 0 408.5-2c-8-25.8-15.4-50-22.4-72z"
-				/>
-				<path
-					fill="#7e5635"
-					d="M228.7 0a123 123 0 0 0-49.1 20.1H237s6.4-10 10.3-13.4a85.2 85.2 0 0 0-29.8 6.5c3.4-6.4 6.1-8.8 11-13.2z"
-				/>
-				<path
-					fill="#f4c38e"
-					d="M213.5 208.8c-63.4 0-63.4 26.9-63.4 60 0 33.2 15.5 66.7 63.4 66.7s63.4-33.5 63.4-66.6c0-33.2 0-60.1-63.4-60.1z"
-				/>
-				<path
-					fill="#202020"
-					d="M245.7 245.4a36 36 0 0 1-32.2 24 36 36 0 0 1-32.2-24c-2.8-10 5.6-16 10.5-17.3 10.5-2.8 33-3.2 43.4 0 4.8 1.4 13.3 7.4 10.5 17.3z"
-				/>
-				<path
-					fill="none"
-					stroke="#333"
-					strokeLinecap="round"
-					strokeMiterlimit="10"
-					strokeWidth="7.5"
-					d="M192 238.5a56.7 56.7 0 0 1 41.9 0"
-				/>
-				<circle cx="289.2" cy="186.9" r="16.9" fill="#202020" />
-				<circle cx="292.3" cy="181.3" r="5.6" fill="#fbfbfb" />
-				<circle cx="136.7" cy="186.9" r="16.9" fill="#202020" />
-				<circle cx="139.8" cy="181.3" r="5.6" fill="#fbfbfb" />
-				<circle cx="100.4" cy="57.8" r="42.3" fill="#7e5635" />
-				<circle cx="100.4" cy="57.8" r="27.5" fill="#644023" />
-				<circle cx="325.6" cy="57.8" r="42.3" fill="#7e5635" />
-				<circle cx="325.6" cy="57.8" r="27.5" fill="#644023" />
+			<g>
+				<g>
+					<circle cx="213" cy="237.3" r="213" fill="url(#a)" />
+					<path
+						fill="#7e5635"
+						d="M395.1 224.7c-7-22.4-13.5-42.5-19.2-59.3-17-50.7-34.3-149-162.3-149-130.8 0-145 98.3-162.2 149-5.7 16.8-12.3 36.9-19.4 59.3l-23 74a213 213 0 0 0 408.5-2c-8-25.8-15.4-50-22.4-72z"
+					/>
+					<path
+						fill="#7e5635"
+						d="M228.7 0a123 123 0 0 0-49.1 20.1H237s6.4-10 10.3-13.4a85.2 85.2 0 0 0-29.8 6.5c3.4-6.4 6.1-8.8 11-13.2z"
+					/>
+					<path
+						fill="#f4c38e"
+						d="M213.5 208.8c-63.4 0-63.4 26.9-63.4 60 0 33.2 15.5 66.7 63.4 66.7s63.4-33.5 63.4-66.6c0-33.2 0-60.1-63.4-60.1z"
+					/>
+					<path
+						fill="#202020"
+						d="M245.7 245.4a36 36 0 0 1-32.2 24 36 36 0 0 1-32.2-24c-2.8-10 5.6-16 10.5-17.3 10.5-2.8 33-3.2 43.4 0 4.8 1.4 13.3 7.4 10.5 17.3z"
+					/>
+					<path
+						fill="none"
+						stroke="#333"
+						strokeLinecap="round"
+						strokeMiterlimit="10"
+						strokeWidth="7.5"
+						d="M192 238.5a56.7 56.7 0 0 1 41.9 0"
+					/>
+					<circle cx="289.2" cy="186.9" r="16.9" fill="#202020" />
+					<circle cx="292.3" cy="181.3" r="5.6" fill="#fbfbfb" />
+					<circle cx="136.7" cy="186.9" r="16.9" fill="#202020" />
+					<circle cx="139.8" cy="181.3" r="5.6" fill="#fbfbfb" />
+					<circle cx="100.4" cy="57.8" r="42.3" fill="#7e5635" />
+					<circle cx="100.4" cy="57.8" r="27.5" fill="#644023" />
+					<circle cx="325.6" cy="57.8" r="42.3" fill="#7e5635" />
+					<circle cx="325.6" cy="57.8" r="27.5" fill="#644023" />
+				</g>
 			</g>
-		</defs>
+		</symbol>
 	</svg>
 );
