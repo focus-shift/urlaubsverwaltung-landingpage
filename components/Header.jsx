7f2a8ac867b02e851356948364b1f9dbf075bddb
Benjamin Seber <seber@synyx.de>
2019-05-22 14:54:19 +0200

    full-width header

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ import SocialMedia from "./SocialMedia";
 
 export default function Header() {
 	return (
-		<header className="bg-blue-900 flex flex-row items-center h-10 overflow-hidden p-2">
+		<header className="bg-blue-900 flex flex-row items-center h-10 p-2">
 			<Navigation />
 			<SocialMedia />
 		</header>
--- components/Page.jsx
+++ components/Page.jsx
@@ -9,14 +9,12 @@ export default function Page({ title, children }) {
 	const pageTitle = ["Urlaubsverwaltung", title].filter(Boolean).join(" · ");
 	return (
 		<React.Fragment>
-			<div className="page flex flex-col flex-1">
-				<Head>
-					{/* for static meta elements see pages/_document.jsx */}
-					<title>{pageTitle}</title>
-				</Head>
-				<Header />
-				<main className="flex-1 mb-12">{children}</main>
-			</div>
+			<Head>
+				{/* for static meta elements see pages/_document.jsx */}
+				<title>{pageTitle}</title>
+			</Head>
+			<Header />
+			<main className="mx-auto max-w-6xl flex-1 mb-12">{children}</main>
 			<Footer />
 		</React.Fragment>
 	);
