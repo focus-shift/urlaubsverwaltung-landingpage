ff8d880ceb3d809388db093e155ecab38e0b2fb4
Benjamin Seber <seber@synyx.de>
2019-05-22 14:48:21 +0200

    full-width footer

--- components/Page.jsx
+++ components/Page.jsx
@@ -8,14 +8,16 @@ import "./style.tailwind.css";
 export default function Page({ title, children }) {
 	const pageTitle = ["Urlaubsverwaltung", title].filter(Boolean).join(" · ");
 	return (
-		<div className="page flex flex-col min-h-full">
-			<Head>
-				{/* for static meta elements see pages/_document.jsx */}
-				<title>{pageTitle}</title>
-			</Head>
-			<Header />
-			<main className="flex-1 mb-12">{children}</main>
+		<React.Fragment>
+			<div className="page flex flex-col flex-1">
+				<Head>
+					{/* for static meta elements see pages/_document.jsx */}
+					<title>{pageTitle}</title>
+				</Head>
+				<Header />
+				<main className="flex-1 mb-12">{children}</main>
+			</div>
 			<Footer />
-		</div>
+		</React.Fragment>
 	);
 }
