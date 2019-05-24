a159c8bf9a9777e7ab3b80a126b5976909565943
Aljona Buchloh <buchloh@synyx.de>
2019-05-24 17:34:51 +0200

    Use consistent form of address (upper cased)

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,8 +23,8 @@ function Stage() {
 				</h1>
 				<div className="mx-auto md:w-2/3 bg-orange-300 p-4 md:p-8 shadow max-w-5xl">
 					<p className="mb-6 text-gray-900 text-l text-center">
-						Der Urlaubär soll dir bei der Urlaubsverwaltung eine Pfote zur Hand
-						legen? Trage dich in unseren Newsletter ein und erfahre als Erstes
+						Der Urlaubär soll Dir bei der Urlaubsverwaltung eine Pfote zur Hand
+						legen? Trage Dich in unseren Newsletter ein und erfahre als Erstes
 						von neuen Entwicklungen in unserer bärenstarken Anwendung.
 					</p>
 					<NewsletterSubscription />
