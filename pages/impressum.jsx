680a63f02b28b9c9e69c9ed863c66e4cec4f49ba
Benjamin Seber <seber@synyx.de>
2019-06-08 14:43:08 +0200

    add impressum

--- pages/impressum.jsx
+++ pages/impressum.jsx
@@ -6,65 +6,52 @@ export default function Impressum() {
 	return (
 		<Page title="Impressum">
 			<div className="mx-auto max-w-6xl">
-				<h1 className="text-2xl font-semibold px-4 mt-12 mb-6">Impressum</h1>
-				<h2 className="px-4 mb-4">
-					Inhalt, fachliche & technische Gestaltung:
-				</h2>
-				<address className="px-4 mb-4">
-					<p className="mb-4">
-						synyx GmbH & Co. KG
-						<br />
-						Gartenstraße 67
-						<br />
-						D-76135 Karlsruhe
-					</p>
-					<p className="mb-4">
-						Telefon: +49 721 203823-0
-						<br />
-						Fax: +49 721 203823-12
-						<br />
-						E-Mail: info@synyx.de
-						<br />
-						https://synyx.de
-					</p>
-					<p className="mb-4">
-						Blog: http://blog.synyx.de
-						<br />
-						IRC: irc://irc.synyx.de #synyx
-					</p>
-				</address>
-				<p className="px-4 mb-4">
-					Sitz der Gesellschaft: Karlsruhe
+				<h1 className="text-3xl mb-6 mt-6">Impressum</h1>
+				<h2 className="text-2xl mb-4">Angaben gemäß § 5 TMG</h2>
+				<p className="mb-2">
+					synyx GmbH & Co. KG
+					<br />
+					Gartenstraße 67
+					<br />
+					76135 Karlsruhe
+				</p>
+				<p className="mb-2">
+					Handelsregister: HRA 104793
 					<br />
 					Registergericht: Amtsgericht Mannheim
-					<br />
-					Handelsregisternummer: HRA 104793
-					<br />
-					USt-IdNr.: DE249264296
-					<br />
-					Komplementärin: synyx Verwaltung GmbH
-					<br />
-					Sitz der Gesellschaft: Karlsruhe
-					<br />
-					Geschäftsführer: Thomas Kraft, Markus Daniel, Joachim Arrasz
-					<br />
-					Handelsregisternummer: HRB 107250
 				</p>
-				<p className="px-4 mb-4">
-					Webmaster: Thomas Kraft
+				<p className="mb-2">
+					<strong>Vertreten durch:</strong>
 					<br />
-					Inhaltlich Verantwortlicher gemäß § 6 MDStV: Thomas Kraft
+					synyx Verwaltung GmbH
 				</p>
-				<p className="px-4">
-					Die allgemeinen Geschäftsbedingungen (AGBs) der synyx GmbH & Co. KG
-					können Sie hier einsehen{" "}
-					<a
-						href="https://synyx.de/assets/downloads/AGB_synyx_17.pdf"
-						download
-						className="text-blue underline"
-					>
-						(PDF-Dokument, 15 Seiten)
-					</a>
+				<p className="mb-2">
+					Diese vertreten durch:
+					<br />
+					Thomas Kraft, Markus Daniel, Joachim Arrasz
+				</p>
+				<p className="mb-6">
+					Handelsregister: HRA 107250
+					<br />
+					Registergericht: Amtsgericht Mannheim
+				</p>
+				<h2 className="text-2xl mb-4">Kontakt</h2>
+				<p className="mb-2">
+					Telefon: +49 721 203823-0
+					<br />
+					Telefax: +49 721 203823-12
+					<br />
+					E-Mail: info@synyx.de
+				</p>
+				<h2 className="text-2xl mb-4">Umsatzsteuer-ID</h2>
+				<p className="mb-2">
+					Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
+					<br />
+					DE249264296
+				</p>
+				<p className="mb-2">
+					Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+					vor einer Verbraucherschlichtungsstelle teilzunehmen.
 				</p>
 			</div>
 		</Page>
