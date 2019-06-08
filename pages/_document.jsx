dca9bf63783a79d68edf588a437ec96bec9991c8
Benjamin Seber <seber@synyx.de>
2019-06-08 22:15:23 +0200

    remove script for prod bundle and go fully static
    * NODE_ENV definition in package json is optional
      nextjs does this implicitly

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,12 +4,48 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const isProd = process.env.NODE_ENV === "production";
+
+class HeadWithoutNextJs extends Head {
+	render() {
+		const res = super.render();
+
+		function transform(node) {
+			// remove all link preloads
+			if (
+				node &&
+				node.type === "link" &&
+				node.props &&
+				node.props.rel === "preload"
+			) {
+				return null;
+			}
+			if (node && node.props && node.props.children) {
+				return {
+					...node,
+					props: {
+						...node.props,
+						children: React.Children.map(node.props.children, transform),
+					},
+				};
+			}
+			if (Array.isArray(node)) {
+				return node.map(transform);
+			}
+
+			return node;
+		}
+
+		return transform(res);
+	}
+}
 export default class MyDocument extends Document {
 	render() {
 		const assetPrefix = process.env.ASSET_PREFIX || "";
+		const MyHead = isProd ? HeadWithoutNextJs : Head;
 		return (
 			<Html lang="de">
-				<Head>
+				<MyHead>
 					<meta
 						name="description"
 						content="Der Urlaubär hilft dir deinen Urlaub zu planen und zu beantragen. Ganz einfach. Ohne Papierkram. Ohne Excellisten."
@@ -26,10 +62,10 @@ export default class MyDocument extends Document {
 						type="image/svg"
 					/>
 					<script defer async src={`${assetPrefix}/static/easteregg.js`} />
-				</Head>
+				</MyHead>
 				<body>
 					<Main />
-					<NextScript />
+					{!isProd && <NextScript />}
 				</body>
 			</Html>
 		);
