e6286ffcae1a11d5cf45ddfc7e57d5ecc90fa476
Benjamin Seber <seber@synyx.de>
2019-06-08 14:34:17 +0200

    add Datenschutz page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,10 +4,19 @@ import Link from "./Link";
 
 export default function Footer() {
 	return (
-		<footer className="bg-gray-800 text-center p-12">
-			<Link href="/impressum" className="text-white">
-				Impressum
-			</Link>
+		<footer className="bg-gray-800 p-12 flex">
+			<ul className="flex flex-row mx-auto">
+				<li className="mx-2">
+					<Link href="/impressum" className="text-white">
+						Impressum
+					</Link>
+				</li>
+				<li className="mx-2">
+					<Link href="/datenschutz" className="text-white">
+						Datenschutz
+					</Link>
+				</li>
+			</ul>
 		</footer>
 	);
 }
---
+++ pages/datenschutz.jsx
@@ -0,0 +1,424 @@
+import React from "react";
+
+import Page from "../components/Page";
+import "./datenschutz.tailwind.css";
+
+export default function Datenschutz() {
+	return (
+		<Page title="Datenschutz">
+			<div className="datenschutz-container">
+				<h1>Datenschutzerklärung</h1>
+				<h2>1. Datenschutz auf einen Blick</h2>
+				<h3>Allgemeine Hinweise</h3>
+				<p>
+					Die folgenden Hinweise geben einen einfachen Überblick darüber, was
+					mit Ihren personenbezogenen Daten passiert, wenn Sie unsere Website
+					besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie
+					persönlich identifiziert werden können. Ausführliche Informationen zum
+					Thema Datenschutz entnehmen Sie unserer unter diesem Text aufgeführten
+					Datenschutzerklärung.
+				</p>
+				<h3>Datenerfassung auf unserer Website</h3>
+				<p>
+					<strong>
+						Wer ist verantwortlich für die Datenerfassung auf dieser Website?
+					</strong>
+				</p>
+				<p>
+					Die Datenverarbeitung auf dieser Website erfolgt durch den
+					Websitebetreiber. Dessen Kontaktdaten können Sie dem Impressum dieser
+					Website entnehmen.
+				</p>
+				<p>
+					<strong>Wie erfassen wir Ihre Daten?</strong>
+				</p>
+				<p>
+					Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese
+					mitteilen. Hierbei kann es sich z. B. um Daten handeln, die Sie in ein
+					Kontaktformular eingeben.
+				</p>
+				<p>
+					Andere Daten werden automatisch beim Besuch der Website durch unsere
+					IT-Systeme erfasst. Das sind vor allem technische Daten (z. B.
+					Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die
+					Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Website
+					betreten.
+				</p>
+				<p>
+					<strong>Wofür nutzen wir Ihre Daten?</strong>
+				</p>
+				<p>
+					Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung
+					der Website zu gewährleisten. Andere Daten können zur Analyse Ihres
+					Nutzerverhaltens verwendet werden.
+				</p>
+				<p>
+					<strong>Welche Rechte haben Sie bezüglich Ihrer Daten?</strong>
+				</p>
+				<p>
+					Sie haben jederzeit das Recht unentgeltlich Auskunft über Herkunft,
+					Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten zu
+					erhalten. Sie haben außerdem ein Recht, die Berichtigung, Sperrung
+					oder Löschung dieser Daten zu verlangen. Hierzu sowie zu weiteren
+					Fragen zum Thema Datenschutz können Sie sich jederzeit unter der im
+					Impressum angegebenen Adresse an uns wenden. Des Weiteren steht Ihnen
+					ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
+				</p>
+				<p>
+					Außerdem haben Sie das Recht, unter bestimmten Umständen die
+					Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu
+					verlangen. Details hierzu entnehmen Sie der Datenschutzerklärung unter
+					Recht auf Einschränkung der Verarbeitung.
+				</p>
+				<h3>Analyse-Tools und Tools von Drittanbietern</h3>
+				<p>
+					Beim Besuch unserer Website kann Ihr Surf-Verhalten statistisch
+					ausgewertet werden. Das geschieht vor allem mit Cookies und mit
+					sogenannten Analyseprogrammen. Die Analyse Ihres Surf-Verhaltens
+					erfolgt in der Regel anonym; das Surf-Verhalten kann nicht zu Ihnen
+					zurückverfolgt werden.
+				</p>
+				<p>
+					Sie können dieser Analyse widersprechen oder sie durch die
+					Nichtbenutzung bestimmter Tools verhindern. Detaillierte Informationen
+					zu diesen Tools und über Ihre Widerspruchsmöglichkeiten finden Sie in
+					der folgenden Datenschutzerklärung.
+				</p>
+				<h2>2. Allgemeine Hinweise und Pflichtinformationen</h2>
+				<h3>Datenschutz</h3>
+				<p>
+					Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten
+					sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und
+					entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser
+					Datenschutzerklärung.
+				</p>
+				<p>
+					Wenn Sie diese Website benutzen, werden verschiedene personenbezogene
+					Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie
+					persönlich identifiziert werden können. Die vorliegende
+					Datenschutzerklärung erläutert, welche Daten wir erheben und wofür wir
+					sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das
+					geschieht.
+				</p>
+				<p>
+					Wir weisen darauf hin, dass die Datenübertragung im Internet (z. B.
+					bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.
+					Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist
+					nicht möglich.
+				</p>
+				<h3>Hinweis zur verantwortlichen Stelle</h3>
+				<p>
+					Die verantwortliche Stelle für die Datenverarbeitung auf dieser
+					Website ist:
+				</p>
+				<address>
+					synyx GmbH & Co. KG
+					<br />
+					Gartenstraße 67
+					<br />
+					D-76135 Karlsruhe
+					<br />
+					Telefon: +49 721 203823-0
+					<br />
+					E-Mail: info@synyx.de
+				</address>
+				<p>
+					Verantwortliche Stelle ist die natürliche oder juristische Person, die
+					allein oder gemeinsam mit anderen über die Zwecke und Mittel der
+					Verarbeitung von personenbezogenen Daten (z. B. Namen, E-Mail-Adressen
+					o. Ä.) entscheidet.
+				</p>
+				<h3>Gesetzlich vorgeschriebener Datenschutzbeauftragter</h3>
+				<p>
+					Wir haben für unser Unternehmen einen Datenschutzbeauftragten
+					bestellt.
+				</p>
+				<address>
+					Daniel Nübling
+					<br />
+					Sophienstr. 120
+					<br />
+					76135 Karlsruhe
+					<br />
+					Telefon: +49 721 203823-0
+					<br />
+					E-Mail: info@synyx.de
+				</address>
+				<h3>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h3>
+				<p>
+					Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen
+					Einwilligung möglich. Sie können eine bereits erteilte Einwilligung
+					jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail
+					an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten
+					Datenverarbeitung bleibt vom Widerruf unberührt.
+				</p>
+				<h3>
+					Widerspruchsrecht gegen die Datenerhebung in besonderen Fällen sowie
+					gegen Direktwerbung (Art. 21 DSGVO)
+				</h3>
+				<p>
+					<strong>
+						Wenn die Datenverarbeitung auf Grundlage von Art. 6 Abs. 1 lit. e
+						oder f DSGVO erfolgt, haben Sie jederzeit das Recht, aus Gründen,
+						die sich aus Ihrer besonderen Situation ergeben, gegen die
+						Verarbeitung Ihrer personenbezogenen Daten Widerspruch einzulegen;
+						dies gilt auch für ein auf diese Bestimmungen gestütztes Profiling.
+						Die jeweilige Rechtsgrundlage, auf denen eine Verarbeitung beruht,
+						entnehmen Sie dieser Datenschutzerklärung. Wenn Sie Widerspruch
+						einlegen, werden wir Ihre betroffenen personenbezogenen Daten nicht
+						mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige
+						Gründe für die Verarbeitung nachweisen, die Ihre Interessen, Rechte
+						und Freiheiten überwiegen oder die Verarbeitung dient der
+						Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen
+						(Widerspruch nach Art. 21 Abs. 1 DSGVO).
+					</strong>
+				</p>
+				<p>
+					<strong>
+						Werden Ihre personenbezogenen Daten verarbeitet, um Direktwerbung zu
+						betreiben, so haben Sie das Recht, jederzeit Widerspruch gegen die
+						Verarbeitung Sie betreffender personenbezogener Daten zum Zwecke
+						derartiger Werbung einzulegen; dies gilt auch für das Profiling,
+						soweit es mit solcher Direktwerbung in Verbindung steht. Wenn Sie
+						widersprechen, werden Ihre personenbezogenen Daten anschließend
+						nicht mehr zum Zwecke der Direktwerbung verwendet (Widerspruch nach
+						Art. 21 Abs. 2 DSGVO).
+					</strong>
+				</p>
+				<h3>Beschwerderecht bei der zuständigen Aufsichtsbehörde</h3>
+				<p>
+					Im Falle von Verstößen gegen die DSGVO steht den Betroffenen ein
+					Beschwerderecht bei einer Aufsichtsbehörde, insbesondere in dem
+					Mitgliedstaat ihres gewöhnlichen Aufenthalts, ihres Arbeitsplatzes
+					oder des Orts des mutmaßlichen Verstoßes zu. Das Beschwerderecht
+					besteht unbeschadet anderweitiger verwaltungsrechtlicher oder
+					gerichtlicher Rechtsbehelfe.
+				</p>
+				<h3>Recht auf Datenübertragbarkeit</h3>
+				<p>
+					Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung
+					oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich
+					oder an einen Dritten in einem gängigen, maschinenlesbaren Format
+					aushändigen zu lassen. Sofern Sie die direkte Übertragung der Daten an
+					einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es
+					technisch machbar ist.
+				</p>
+				<h3>SSL- bzw. TLS-Verschlüsselung</h3>
+				<p>
+					Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der
+					Übertragung vertraulicher Inhalte, wie zum Beispiel Bestellungen oder
+					Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL- bzw.
+					TLS-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie
+					daran, dass die Adresszeile des Browsers von http:// auf https://
+					wechselt und an dem Schloss-Symbol in Ihrer Browserzeile.
+				</p>
+				<p>
+					Wenn die SSL- bzw. TLS-Verschlüsselung aktiviert ist, können die
+					Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen
+					werden.
+				</p>
+				<h3>Auskunft, Sperrung, Löschung und Berichtigung</h3>
+				<p>
+					Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit
+					das Recht auf unentgeltliche Auskunft über Ihre gespeicherten
+					personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck
+					der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung
+					oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema
+					personenbezogene Daten können Sie sich jederzeit unter der im
+					Impressum angegebenen Adresse an uns wenden.
+				</p>
+				<h3>Recht auf Einschränkung der Verarbeitung</h3>
+				<p>
+					Sie haben das Recht, die Einschränkung der Verarbeitung Ihrer
+					personenbezogenen Daten zu verlangen. Hierzu können Sie sich jederzeit
+					unter der im Impressum angegebenen Adresse an uns wenden. Das Recht
+					auf Einschränkung der Verarbeitung besteht in folgenden Fällen:
+				</p>
+				<ul>
+					<li>
+						Wenn Sie die Richtigkeit Ihrer bei uns gespeicherten
+						personenbezogenen Daten bestreiten, benötigen wir in der Regel Zeit,
+						um dies zu überprüfen. Für die Dauer der Prüfung haben Sie das
+						Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen
+						Daten zu verlangen.
+					</li>
+					<li>
+						Wenn die Verarbeitung Ihrer personenbezogenen Daten unrechtmäßig
+						geschah/geschieht, können Sie statt der Löschung die Einschränkung
+						der Datenverarbeitung verlangen.
+					</li>
+					<li>
+						Wenn wir Ihre personenbezogenen Daten nicht mehr benötigen, Sie sie
+						jedoch zur Ausübung, Verteidigung oder Geltendmachung von
+						Rechtsansprüchen benötigen, haben Sie das Recht, statt der Löschung
+						die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu
+						verlangen.
+					</li>
+					<li>
+						Wenn Sie einen Widerspruch nach Art. 21 Abs. 1 DSGVO eingelegt
+						haben, muss eine Abwägung zwischen Ihren und unseren Interessen
+						vorgenommen werden. Solange noch nicht feststeht, wessen Interessen
+						überwiegen, haben Sie das Recht, die Einschränkung der Verarbeitung
+						Ihrer personenbezogenen Daten zu verlangen.
+					</li>
+				</ul>
+				<p>
+					Wenn Sie die Verarbeitung Ihrer personenbezogenen Daten eingeschränkt
+					haben, dürfen diese Daten  von ihrer Speicherung abgesehen  nur mit
+					Ihrer Einwilligung oder zur Geltendmachung, Ausübung oder Verteidigung
+					von Rechtsansprüchen oder zum Schutz der Rechte einer anderen
+					natürlichen oder juristischen Person oder aus Gründen eines wichtigen
+					öffentlichen Interesses der Europäischen Union oder eines
+					Mitgliedstaats verarbeitet werden.
+				</p>
+				<h2>3. Datenerfassung auf unserer Website</h2>
+				<h3>Server-Log-Dateien</h3>
+				<p>
+					Der Provider der Seiten erhebt und speichert automatisch Informationen
+					in so genannten Server-Log-Dateien, die Ihr Browser automatisch an uns
+					übermittelt. Dies sind:
+				</p>
+				<ul>
+					<li>Browsertyp und Browserversion</li>
+					<li>verwendetes Betriebssystem</li>
+					<li>Referrer URL</li>
+					<li>Hostname des zugreifenden Rechners</li>
+					<li>Uhrzeit der Serveranfrage</li>
+					<li>IP-Adresse</li>
+				</ul>
+				<p>
+					Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht
+					vorgenommen.
+				</p>
+				<p>
+					Die Erfassung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1
+					lit. f DSGVO. Der Websitebetreiber hat ein berechtigtes Interesse an
+					der technisch fehlerfreien Darstellung und der Optimierung seiner
+					Website hierzu müssen die Server-Log-Files erfasst werden.
+				</p>
+				<h2>4. Newsletter</h2>
+				<h3>Newsletterdaten</h3>
+				<p>
+					Wenn Sie den auf der Website angebotenen Newsletter beziehen möchten,
+					benötigen wir von Ihnen eine E-Mail-Adresse sowie Informationen,
+					welche uns die Überprüfung gestatten, dass Sie der Inhaber der
+					angegebenen E-Mail-Adresse sind und mit dem Empfang des Newsletters
+					einverstanden sind. Weitere Daten werden nicht bzw. nur auf
+					freiwilliger Basis erhoben. Diese Daten verwenden wir ausschließlich
+					für den Versand der angeforderten Informationen und geben diese nicht
+					an Dritte weiter.
+				</p>
+				<p>
+					Die Verarbeitung der in das Newsletteranmeldeformular eingegebenen
+					Daten erfolgt ausschließlich auf Grundlage Ihrer Einwilligung (Art. 6
+					Abs. 1 lit. a DSGVO). Die erteilte Einwilligung zur Speicherung der
+					Daten, der E-Mail-Adresse sowie deren Nutzung zum Versand des
+					Newsletters können Sie jederzeit widerrufen, etwa über den
+					Austragen-Link im Newsletter. Die Rechtmäßigkeit der bereits erfolgten
+					Datenverarbeitungsvorgänge bleibt vom Widerruf unberührt.
+				</p>
+				<p>
+					Die von Ihnen zum Zwecke des Newsletter-Bezugs bei uns hinterlegten
+					Daten werden von uns bis zu Ihrer Austragung aus dem Newsletter
+					gespeichert und nach der Abbestellung des Newsletters gelöscht. Daten,
+					die zu anderen Zwecken bei uns gespeichert wurden bleiben hiervon
+					unberührt.
+				</p>
+				<h3>MailChimp</h3>
+				<p>
+					Diese Website nutzt die Dienste von MailChimp für den Versand von
+					Newslettern. Anbieter ist die Rocket Science Group LLC, 675 Ponce De
+					Leon Ave NE, Suite 5000, Atlanta, GA 30308, USA.
+				</p>
+				<p>
+					MailChimp ist ein Dienst, mit dem u.a. der Versand von Newslettern
+					organisiert und analysiert werden kann. Wenn Sie Daten zum Zwecke des
+					Newsletterbezugs eingeben (z. B. E-Mail-Adresse), werden diese auf den
+					Servern von MailChimp in den USA gespeichert.
+				</p>
+				<p>
+					MailChimp verfügt über eine Zertifizierung nach dem
+					EU-US-Privacy-Shield. Der Privacy-Shield ist ein Übereinkommen
+					zwischen der Europäischen Union (EU) und den USA, das die Einhaltung
+					europäischer Datenschutzstandards in den USA gewährleisten soll.
+				</p>
+				<p>
+					Mit Hilfe von MailChimp können wir unsere Newsletterkampagnen
+					analysieren. Wenn Sie eine mit MailChimp versandte E-Mail öffnen,
+					verbindet sich eine in der E-Mail enthaltene Datei (sog. web-beacon)
+					mit den Servern von MailChimp in den USA. So kann festgestellt werden,
+					ob eine Newsletter-Nachricht geöffnet und welche Links ggf. angeklickt
+					wurden. Außerdem werden technische Informationen erfasst (z. B.
+					Zeitpunkt des Abrufs, IP-Adresse, Browsertyp und Betriebssystem).
+					Diese Informationen können nicht dem jeweiligen Newsletter-Empfänger
+					zugeordnet werden. Sie dienen ausschließlich der statistischen Analyse
+					von Newsletterkampagnen. Die Ergebnisse dieser Analysen können genutzt
+					werden, um künftige Newsletter besser an die Interessen der Empfänger
+					anzupassen.
+				</p>
+				<p>
+					Wenn Sie keine Analyse durch MailChimp wollen, müssen Sie den
+					Newsletter abbestellen. Hierfür stellen wir in jeder
+					Newsletternachricht einen entsprechenden Link zur Verfügung. Des
+					Weiteren können Sie den Newsletter auch direkt auf der Website
+					abbestellen.
+				</p>
+				<p>
+					Die Datenverarbeitung erfolgt auf Grundlage Ihrer Einwilligung (Art. 6
+					Abs. 1 lit. a DSGVO). Sie können diese Einwilligung jederzeit
+					widerrufen, indem Sie den Newsletter abbestellen. Die Rechtmäßigkeit
+					der bereits erfolgten Datenverarbeitungsvorgänge bleibt vom Widerruf
+					unberührt.
+				</p>
+				<p>
+					Die von Ihnen zum Zwecke des Newsletter-Bezugs bei uns hinterlegten
+					Daten werden von uns bis zu Ihrer Austragung aus dem Newsletter
+					gespeichert und nach der Abbestellung des Newsletters sowohl von
+					unseren Servern als auch von den Servern von MailChimp gelöscht.
+					Daten, die zu anderen Zwecken bei uns gespeichert wurden bleiben
+					hiervon unberührt.
+				</p>
+				<p>
+					Näheres entnehmen Sie den Datenschutzbestimmungen von MailChimp
+					unter:&nbsp;
+					<a
+						href="https://mailchimp.com/legal/terms/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="underline"
+					>
+						https://mailchimp.com/legal/terms/
+					</a>
+					.
+				</p>
+				<p>
+					<strong>Abschluss eines Data-Processing-Agreements</strong>
+				</p>
+				<p>
+					MailChimp hat ein sog. Data-Processing-Agreement in seine AGBs
+					aufgenommen, in dem sich MailChimp verpflichtet, die Daten unserer
+					Kunden zu schützen und sie nicht an Dritte weiterzugeben.
+				</p>
+				<p>
+					Das Data-Processing-Agreement kann online eingesehen werden:
+					<a
+						href="https://mailchimp.com/legal/data-processing-addendum/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="underline"
+					>
+						https://mailchimp.com/legal/data-processing-addendum/
+					</a>
+				</p>
+				<h2>5. Plugins und Tools</h2>
+				<h3>Google Web Fonts</h3>
+				<p>
+					Diese Seite nutzt zur einheitlichen Darstellung von Schriftarten so
+					genannte Web Fonts, die von Google bereitgestellt werden. Die Google
+					Fonts sind lokal installiert. Eine Verbindung zu Servern von Google
+					findet dabei nicht statt.
+				</p>
+			</div>
+		</Page>
+	);
+}
