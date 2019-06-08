11c7aed7a0097209e77ed764fa551a2d726f19c6
Benjamin Seber <seber@synyx.de>
2019-06-08 21:58:36 +0200

    remove obsolete components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-import Navigation from "./Navigation";
 import SocialMedia from "./SocialMedia";
 
 export default function Header() {
 	return (
-		<header className="bg-blue-900 flex flex-row items-center h-10 p-2">
-			<Navigation />
+		<header className="bg-blue-900 flex flex-row items-center justify-end h-10 p-2">
 			<SocialMedia />
 		</header>
 	);
