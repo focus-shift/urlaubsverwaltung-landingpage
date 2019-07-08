afd8b735eb5a786ce7686a3a3edfa12979716bdc
Benjamin Seber <seber@synyx.de>
2019-07-08 20:55:47 +0200

    add visually hidden link to frontpage in the footer navigation
    dunno why ¯\_(ツ)_/¯
    link is also on the top. but maybe someone jumps to the footer navigation and misses the frontpage link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,13 @@ import Link from "./Link";
 export default function Footer() {
 	return (
 		<footer className="bg-gray-800 p-12 flex">
+			<h2 className="visually-hidden">Navigation</h2>
 			<ul className="flex flex-row mx-auto">
+				<li className="visually-hidden">
+					<Link href="/" className="text-white">
+						Startseite
+					</Link>
+				</li>
 				<li className="mx-2">
 					<Link href="/impressum" className="text-white">
 						Impressum
