b5c49f85b011da28da60b09eba09e49d9b4d1ab5
Benjamin Seber <seber@synyx.de>
2019-07-08 19:08:22 +0200

    add comment which font to use for which font-weight

--- components/Page.jsx
+++ components/Page.jsx
@@ -26,6 +26,17 @@ export default function Page({ title, children }) {
 
 const assetPrefix = process.env.ASSET_PREFIX || "";
 
+/*
+100    Extra Light or Ultra Light
+200    Light or Thin
+300    Book or Demi
+400    Normal or Regular
+500    Medium
+600    Semibold, Demibold
+700    Bold
+800    Black, Extra Bold or Heavy
+900    Extra Black, Fat, Poster or Ultra Black
+*/
 const fonts = css`
 	@font-face {
 		font-family: "Open Sans";
