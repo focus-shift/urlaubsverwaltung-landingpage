fa77fe2d29ed74b417e7c74cda62d0a40459f506
Benjamin Seber <seber@synyx.de>
2019-07-09 09:25:01 +0200

    use Link component to link to start page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
+import Link from "./Link";
 import BearIcon from "./BearIcon";
 import SocialMedia from "./SocialMedia";
 
 export default function Header() {
-	const basePath = process.env.BASE_PATH || "/";
 	return (
 		<header className="bg-blue-900 flex flex-row items-center justify-end h-10 p-2">
-			<a href={basePath} className="flex flex-row items-center">
+			<Link href="/" className="flex flex-row items-center">
 				<BearIcon width={32} height={32} ariaHidden />
 				<span className="text-white pl-2">Urlaubsverwaltung</span>
 				<span className="visually-hidden">&nbsp;Startseite</span>
-			</a>
+			</Link>
 			<div className="ml-auto">
 				<SocialMedia />
 			</div>
