cf8c20914f30e1e08ec13d31dbb6aaa362c2151c
Benjamin Seber <seber@synyx.de>
2019-07-09 09:22:43 +0200

    use env provided basePath to link to start page

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,9 +4,10 @@ import BearIcon from "./BearIcon";
 import SocialMedia from "./SocialMedia";
 
 export default function Header() {
+	const basePath = process.env.BASE_PATH || "/";
 	return (
 		<header className="bg-blue-900 flex flex-row items-center justify-end h-10 p-2">
-			<a href="/" className="flex flex-row items-center">
+			<a href={basePath} className="flex flex-row items-center">
 				<BearIcon width={32} height={32} ariaHidden />
 				<span className="text-white pl-2">Urlaubsverwaltung</span>
 				<span className="visually-hidden">&nbsp;Startseite</span>
