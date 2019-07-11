05ce4385cd36b2f8de80c8eb4f005ae63ea7d84c
Aljona Buchloh <buchloh@synyx.de>
2019-07-11 12:47:23 +0200

    Update Datenschutz page due to switch to MailJet

--- pages/datenschutz.tsx
+++ pages/datenschutz.tsx
@@ -12,13 +12,13 @@ export default function Datenschutz() {
 				<h3>Allgemeine Hinweise</h3>
 				<p>
 					Die folgenden Hinweise geben einen einfachen Überblick darüber, was
-					mit Ihren personenbezogenen Daten passiert, wenn Sie unsere Website
+					mit Ihren personenbezogenen Daten passiert, wenn Sie diese Website
 					besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie
 					persönlich identifiziert werden können. Ausführliche Informationen zum
 					Thema Datenschutz entnehmen Sie unserer unter diesem Text aufgeführten
 					Datenschutzerklärung.
 				</p>
-				<h3>Datenerfassung auf unserer Website</h3>
+				<h3>Datenerfassung auf dieser Website</h3>
 				<p>
 					<strong>
 						Wer ist verantwortlich für die Datenerfassung auf dieser Website?
@@ -41,15 +41,15 @@ export default function Datenschutz() {
 					Andere Daten werden automatisch beim Besuch der Website durch unsere
 					IT-Systeme erfasst. Das sind vor allem technische Daten (z. B.
 					Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die
-					Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Website
+					Erfassung dieser Daten erfolgt automatisch, sobald Sie diese Website
 					betreten.
 				</p>
 				<p>
 					<strong>Wofür nutzen wir Ihre Daten?</strong>
 				</p>
 				<p>
-					Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung
-					der Website zu gewährleisten. Andere Daten können zur Analyse Ihres
+					Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der
+					Website zu gewährleisten. Andere Daten können zur Analyse Ihres
 					Nutzerverhaltens verwendet werden.
 				</p>
 				<p>
@@ -58,21 +58,21 @@ export default function Datenschutz() {
 				<p>
 					Sie haben jederzeit das Recht unentgeltlich Auskunft über Herkunft,
 					Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten zu
-					erhalten. Sie haben außerdem ein Recht, die Berichtigung, Sperrung
-					oder Löschung dieser Daten zu verlangen. Hierzu sowie zu weiteren
-					Fragen zum Thema Datenschutz können Sie sich jederzeit unter der im
-					Impressum angegebenen Adresse an uns wenden. Des Weiteren steht Ihnen
-					ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
+					erhalten. Sie haben außerdem ein Recht, die Berichtigung oder Löschung
+					dieser Daten zu verlangen. Hierzu sowie zu weiteren Fragen zum Thema
+					Datenschutz können Sie sich jederzeit unter der im Impressum
+					angegebenen Adresse an uns wenden. Des Weiteren steht Ihnen ein
+					Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
 				</p>
 				<p>
-					Außerdem haben Sie das Recht, unter bestimmten Umständen die
-					Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu
-					verlangen. Details hierzu entnehmen Sie der Datenschutzerklärung unter
-					Recht auf Einschränkung der Verarbeitung.
+					Außerdem haben Sie das Recht, unter bestimmten Umständen die Einschränkung
+					der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Details
+					hierzu entnehmen Sie der Datenschutzerklärung unter "Recht auf
+					Einschränkung der Verarbeitung".
 				</p>
 				<h3>Analyse-Tools und Tools von Drittanbietern</h3>
 				<p>
-					Beim Besuch unserer Website kann Ihr Surf-Verhalten statistisch
+					Beim Besuch dieser Website kann Ihr Surf-Verhalten statistisch
 					ausgewertet werden. Das geschieht vor allem mit Cookies und mit
 					sogenannten Analyseprogrammen. Die Analyse Ihres Surf-Verhaltens
 					erfolgt in der Regel anonym; das Surf-Verhalten kann nicht zu Ihnen
@@ -142,7 +142,7 @@ export default function Datenschutz() {
 					<br />
 					Telefon: <a href="tel:+497212038230">+49 721 203823-0</a>
 					<br />
-					E-Mail: <a href="mailto:info@synyx.de">info@synyx.de</a>
+					E-Mail: <a href="mailto:datenschutzbeauftragter@synyx.de">datenschutzbeauftragter@synyx.de</a>
 				</address>
 				<h3>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h3>
 				<p>
@@ -157,33 +157,29 @@ export default function Datenschutz() {
 					gegen Direktwerbung (Art. 21 DSGVO)
 				</h3>
 				<p>
-					<strong>
-						Wenn die Datenverarbeitung auf Grundlage von Art. 6 Abs. 1 lit. e
-						oder f DSGVO erfolgt, haben Sie jederzeit das Recht, aus Gründen,
-						die sich aus Ihrer besonderen Situation ergeben, gegen die
-						Verarbeitung Ihrer personenbezogenen Daten Widerspruch einzulegen;
-						dies gilt auch für ein auf diese Bestimmungen gestütztes Profiling.
-						Die jeweilige Rechtsgrundlage, auf denen eine Verarbeitung beruht,
-						entnehmen Sie dieser Datenschutzerklärung. Wenn Sie Widerspruch
-						einlegen, werden wir Ihre betroffenen personenbezogenen Daten nicht
-						mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige
-						Gründe für die Verarbeitung nachweisen, die Ihre Interessen, Rechte
-						und Freiheiten überwiegen oder die Verarbeitung dient der
-						Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen
-						(Widerspruch nach Art. 21 Abs. 1 DSGVO).
-					</strong>
+					WENN DIE DATENVERARBEITUNG AUF GRUNDLAGE VON ART. 6 ABS. 1 LIT. E ODER
+					F DSGVO ERFOLGT, HABEN SIE JEDERZEIT DAS RECHT, AUS GRÜNDEN, DIE SICH
+					AUS IHRER BESONDEREN SITUATION ERGEBEN, GEGEN DIE VERARBEITUNG IHRER
+					PERSONENBEZOGENEN DATEN WIDERSPRUCH EINZULEGEN; DIES GILT AUCH FÜR EIN
+					AUF DIESE BESTIMMUNGEN GESTÜTZTES PROFILING. DIE JEWEILIGE
+					RECHTSGRUNDLAGE, AUF DENEN EINE VERARBEITUNG BERUHT, ENTNEHMEN SIE
+					DIESER DATENSCHUTZERKLÄRUNG. WENN SIE WIDERSPRUCH EINLEGEN, WERDEN WIR
+					IHRE BETROFFENEN PERSONENBEZOGENEN DATEN NICHT MEHR VERARBEITEN, ES
+					SEI DENN, WIR KÖNNEN ZWINGENDE SCHUTZWÜRDIGE GRÜNDE FÜR DIE
+					VERARBEITUNG NACHWEISEN, DIE IHRE INTERESSEN, RECHTE UND FREIHEITEN
+					ÜBERWIEGEN ODER DIE VERARBEITUNG DIENT DER GELTENDMACHUNG, AUSÜBUNG
+					ODER VERTEIDIGUNG VON RECHTSANSPRÜCHEN (WIDERSPRUCH NACH ART. 21 ABS.
+					1 DSGVO).
 				</p>
 				<p>
-					<strong>
-						Werden Ihre personenbezogenen Daten verarbeitet, um Direktwerbung zu
-						betreiben, so haben Sie das Recht, jederzeit Widerspruch gegen die
-						Verarbeitung Sie betreffender personenbezogener Daten zum Zwecke
-						derartiger Werbung einzulegen; dies gilt auch für das Profiling,
-						soweit es mit solcher Direktwerbung in Verbindung steht. Wenn Sie
-						widersprechen, werden Ihre personenbezogenen Daten anschließend
-						nicht mehr zum Zwecke der Direktwerbung verwendet (Widerspruch nach
-						Art. 21 Abs. 2 DSGVO).
-					</strong>
+					WERDEN IHRE PERSONENBEZOGENEN DATEN VERARBEITET, UM DIREKTWERBUNG ZU
+					BETREIBEN, SO HABEN SIE DAS RECHT, JEDERZEIT WIDERSPRUCH GEGEN DIE
+					VERARBEITUNG SIE BETREFFENDER PERSONENBEZOGENER DATEN ZUM ZWECKE
+					DERARTIGER WERBUNG EINZULEGEN; DIES GILT AUCH FÜR DAS PROFILING,
+					SOWEIT ES MIT SOLCHER DIREKTWERBUNG IN VERBINDUNG STEHT. WENN SIE
+					WIDERSPRECHEN, WERDEN IHRE PERSONENBEZOGENEN DATEN ANSCHLIESSEND NICHT
+					MEHR ZUM ZWECKE DER DIREKTWERBUNG VERWENDET (WIDERSPRUCH NACH ART. 21
+					ABS. 2 DSGVO).
 				</p>
 				<h3>Beschwerderecht bei der zuständigen Aufsichtsbehörde</h3>
 				<p>
@@ -209,7 +205,7 @@ export default function Datenschutz() {
 					Übertragung vertraulicher Inhalte, wie zum Beispiel Bestellungen oder
 					Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL- bzw.
 					TLS-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie
-					daran, dass die Adresszeile des Browsers von http:// auf https://
+					daran, dass die Adresszeile des Browsers von "http://" auf "https://"
 					wechselt und an dem Schloss-Symbol in Ihrer Browserzeile.
 				</p>
 				<p>
@@ -217,13 +213,13 @@ export default function Datenschutz() {
 					Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen
 					werden.
 				</p>
-				<h3>Auskunft, Sperrung, Löschung und Berichtigung</h3>
+				<h3>Auskunft, Löschung und Berichtigung</h3>
 				<p>
 					Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit
 					das Recht auf unentgeltliche Auskunft über Ihre gespeicherten
 					personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck
-					der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung
-					oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema
+					der Datenverarbeitung und ggf. ein Recht auf Berichtigung oder
+					Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema
 					personenbezogene Daten können Sie sich jederzeit unter der im
 					Impressum angegebenen Adresse an uns wenden.
 				</p>
@@ -264,14 +260,14 @@ export default function Datenschutz() {
 				</ul>
 				<p>
 					Wenn Sie die Verarbeitung Ihrer personenbezogenen Daten eingeschränkt
-					haben, dürfen diese Daten  von ihrer Speicherung abgesehen  nur mit
+					haben, dürfen diese Daten (von ihrer Speicherung abgesehen) nur mit
 					Ihrer Einwilligung oder zur Geltendmachung, Ausübung oder Verteidigung
 					von Rechtsansprüchen oder zum Schutz der Rechte einer anderen
 					natürlichen oder juristischen Person oder aus Gründen eines wichtigen
 					öffentlichen Interesses der Europäischen Union oder eines
 					Mitgliedstaats verarbeitet werden.
 				</p>
-				<h2>3. Datenerfassung auf unserer Website</h2>
+				<h2>3. Datenerfassung auf dieser Website</h2>
 				<h3>Server-Log-Dateien</h3>
 				<p>
 					Der Provider der Seiten erhebt und speichert automatisch Informationen
@@ -294,7 +290,7 @@ export default function Datenschutz() {
 					Die Erfassung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1
 					lit. f DSGVO. Der Websitebetreiber hat ein berechtigtes Interesse an
 					der technisch fehlerfreien Darstellung und der Optimierung seiner
-					Website hierzu müssen die Server-Log-Files erfasst werden.
+					Website. Hierzu müssen die Server-Log-Files erfasst werden.
 				</p>
 				<h2 id="newsletter">4. Newsletter</h2>
 				<h3>Newsletterdaten</h3>
@@ -314,101 +310,138 @@ export default function Datenschutz() {
 					Abs. 1 lit. a DSGVO). Die erteilte Einwilligung zur Speicherung der
 					Daten, der E-Mail-Adresse sowie deren Nutzung zum Versand des
 					Newsletters können Sie jederzeit widerrufen, etwa über den
-					Austragen-Link im Newsletter. Die Rechtmäßigkeit der bereits erfolgten
-					Datenverarbeitungsvorgänge bleibt vom Widerruf unberührt.
+					"Austragen"-Link im Newsletter. Die Rechtmäßigkeit der bereits
+					erfolgten Datenverarbeitungsvorgänge bleibt vom Widerruf unberührt.
 				</p>
 				<p>
 					Die von Ihnen zum Zwecke des Newsletter-Bezugs bei uns hinterlegten
-					Daten werden von uns bis zu Ihrer Austragung aus dem Newsletter
-					gespeichert und nach der Abbestellung des Newsletters gelöscht. Daten,
-					die zu anderen Zwecken bei uns gespeichert wurden bleiben hiervon
-					unberührt.
-				</p>
-				<h3>MailChimp</h3>
-				<p>
-					Diese Website nutzt die Dienste von MailChimp für den Versand von
-					Newslettern. Anbieter ist die Rocket Science Group LLC, 675 Ponce De
-					Leon Ave NE, Suite 5000, Atlanta, GA 30308, USA.
+					Daten werden von uns bis zu Ihrer Austragung aus dem Newsletter bei
+					uns bzw. dem Newsletterdiensteanbieter gespeichert und nach der
+					Abbestellung des Newsletters aus der Newsletterverteilerliste
+					gelöscht. Daten, die zu anderen Zwecken bei uns gespeichert wurden
+					bleiben hiervon unberührt.
 				</p>
 				<p>
-					MailChimp ist ein Dienst, mit dem u.a. der Versand von Newslettern
-					organisiert und analysiert werden kann. Wenn Sie Daten zum Zwecke des
-					Newsletterbezugs eingeben (z. B. E-Mail-Adresse), werden diese auf den
-					Servern von MailChimp in den USA gespeichert.
+					Nach Ihrer Austragung aus der Newsletterverteilerliste wird Ihre
+					E-Mail-Adresse bei uns bzw. dem Newsletterdiensteanbieter ggf. in
+					einer Blacklist gespeichert, um künftige Mailings zu verhindern. Die
+					Daten aus der Blacklist werden nur für diesen Zweck verwendet und
+					nicht mit anderen Daten zusammengeführt. Dies dient sowohl Ihrem
+					Interesse als auch unserem Interesse an der Einhaltung der
+					gesetzlichen Vorgaben beim Versand von Newslettern (berechtigtes
+					Interesse im Sinne des Art. 6 Abs. 1 lit. f DSGVO). Die Speicherung in
+					der Blacklist ist zeitlich nicht befristet.
+					<br />
+					<br />
+					<strong>
+						Sie können der Speicherung widersprechen, sofern Ihre Interessen
+						unser berechtigtes Interesse überwiegen.
+					</strong>
+				</p>
+				<h3>MailJet</h3>
+				<p>
+					Diese Website nutzt Mailjet für den Versand von Newslettern. Anbieter
+					ist die Mailjet SAS (Global HQ) Büro- und Postadresse Paris:
+				</p>
+				<address>
+					13-13 bis
+					<br />
+					rue de l&rsquo;Aubrac
+					<br />
+					75012 Paris
+					<br />
+					France
+				</address>
+				<p>
+					Mailjet ist ein Dienst, mit dem u.a. der Versand von Newslettern
+					organisiert und analysiert werden kann. Die von Ihnen zum Zwecke des
+					Newsletterbezugs eingegeben Daten werden auf den Servern von Mailjet
+					in Deutschland gespeichert.
 				</p>
 				<p>
-					MailChimp verfügt über eine Zertifizierung nach dem
-					EU-US-Privacy-Shield. Der Privacy-Shield ist ein Übereinkommen
-					zwischen der Europäischen Union (EU) und den USA, das die Einhaltung
-					europäischer Datenschutzstandards in den USA gewährleisten soll.
+					Wenn Sie keine Analyse durch Mailjet wollen, müssen Sie den Newsletter
+					abbestellen. Hierfür stellen wir in jeder Newsletternachricht einen
+					entsprechenden Link zur Verfügung. Des Weiteren können Sie den
+					Newsletter auch direkt auf der Website abbestellen.
 				</p>
 				<p>
-					Mit Hilfe von MailChimp können wir unsere Newsletterkampagnen
-					analysieren. Wenn Sie eine mit MailChimp versandte E-Mail öffnen,
-					verbindet sich eine in der E-Mail enthaltene Datei (sog. web-beacon)
-					mit den Servern von MailChimp in den USA. So kann festgestellt werden,
-					ob eine Newsletter-Nachricht geöffnet und welche Links ggf. angeklickt
-					wurden. Außerdem werden technische Informationen erfasst (z. B.
-					Zeitpunkt des Abrufs, IP-Adresse, Browsertyp und Betriebssystem).
-					Diese Informationen können nicht dem jeweiligen Newsletter-Empfänger
-					zugeordnet werden. Sie dienen ausschließlich der statistischen Analyse
-					von Newsletterkampagnen. Die Ergebnisse dieser Analysen können genutzt
-					werden, um künftige Newsletter besser an die Interessen der Empfänger
-					anzupassen.
+					<strong>Datenanalyse durch Mailjet</strong>
 				</p>
 				<p>
-					Wenn Sie keine Analyse durch MailChimp wollen, müssen Sie den
-					Newsletter abbestellen. Hierfür stellen wir in jeder
-					Newsletternachricht einen entsprechenden Link zur Verfügung. Des
-					Weiteren können Sie den Newsletter auch direkt auf der Website
-					abbestellen.
+					Mit Hilfe von Mailjet ist es uns möglich, unsere Newsletter-Kampagnen
+					zu analysieren. So können wir z.B. sehen, ob eine Newsletter-Nachricht
+					geöffnet wurde und welche Links ggf. angeklickt wurden. Auf diese
+					Weise können wir u.a. feststellen, welche Links besonders oft
+					angeklickt wurden.
+				</p>
+				<p>
+					Außerdem können wir erkennen, ob nach dem Öffnen/ Anklicken bestimmte
+					vorher definierte Aktionen durchgeführt wurden (Conversion-Rate). Wir
+					können so z.B. erkennen, ob Sie nach dem Anklicken des Newsletters
+					einen Kauf getätigt haben.
+				</p>
+				<p>
+					Mailjet ermöglicht es uns auch, die Newsletter-Empfänger anhand
+					verschiedener Kategorien zu unterteilen ("clustern"). Dabei lassen
+					sich die Newsletterempfänger z.B. nach Alter, Geschlecht oder Wohnort
+					unterteilen. Auf diese Weise lassen sich die Newsletter besser an die
+					jeweiligen Zielgruppen anpassen.
+				</p>
+				<p>
+					Ausführliche Informationen zu den Funktionen von Mailjet entnehmen Sie
+					folgendem Link:
+					<a href="https://www.mailjet.de/funktion/" target="_blank"
+					rel="noopener noreferrer">https://www.mailjet.de/funktion/</a>.
+				</p>
+				<p>
+					Die Datenschutzerklärung von Mailjet finden Sie unter:
+					<a href="https://www.mailjet.de/sicherheit-datenschutz/"
+					target="_blank" rel="noopener
+					noreferrer">https://www.mailjet.de/sicherheit-datenschutz/</a>.
+				</p>
+				<p>
 				</p>
 				<p>
 					Die Datenverarbeitung erfolgt auf Grundlage Ihrer Einwilligung (Art. 6
 					Abs. 1 lit. a DSGVO). Sie können diese Einwilligung jederzeit
-					widerrufen, indem Sie den Newsletter abbestellen. Die Rechtmäßigkeit
-					der bereits erfolgten Datenverarbeitungsvorgänge bleibt vom Widerruf
-					unberührt.
+					widerrufen. Die Rechtmäßigkeit der bereits erfolgten
+					Datenverarbeitungsvorgänge bleibt vom Widerruf unberührt.
+				</p>
+				<p>
+					<strong>Speicherdauer</strong>
 				</p>
 				<p>
 					Die von Ihnen zum Zwecke des Newsletter-Bezugs bei uns hinterlegten
-					Daten werden von uns bis zu Ihrer Austragung aus dem Newsletter
-					gespeichert und nach der Abbestellung des Newsletters sowohl von
-					unseren Servern als auch von den Servern von MailChimp gelöscht.
-					Daten, die zu anderen Zwecken bei uns gespeichert wurden bleiben
-					hiervon unberührt.
+					Daten werden von uns bis zu Ihrer Austragung aus dem Newsletter bei
+					uns bzw. dem Newsletterdiensteanbieter gespeichert und nach der
+					Abbestellung des Newsletters aus der Newsletterverteilerliste
+					gelöscht. Daten, die zu anderen Zwecken bei uns gespeichert wurden
+					bleiben hiervon unberührt.
 				</p>
 				<p>
-					Näheres entnehmen Sie den Datenschutzbestimmungen von MailChimp
-					unter:&nbsp;
-					<a
-						href="https://mailchimp.com/legal/terms/"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="underline"
-					>
-						https://mailchimp.com/legal/terms/
-					</a>
-					.
+					Nach Ihrer Austragung aus der Newsletterverteilerliste wird Ihre
+					E-Mail-Adresse bei uns bzw. dem Newsletterdiensteanbieter ggf. in
+					einer Blacklist gespeichert, um künftige Mailings zu verhindern. Die
+					Daten aus der Blacklist werden nur für diesen Zweck verwendet und
+					nicht mit anderen Daten zusammengeführt. Dies dient sowohl Ihrem
+					Interesse als auch unserem Interesse an der Einhaltung der
+					gesetzlichen Vorgaben beim Versand von Newslettern (berechtigtes
+					Interesse im Sinne des Art. 6 Abs. 1 lit. f DSGVO). Die Speicherung in
+					der Blacklist ist zeitlich nicht befristet.
+					<br />
+					<br />
+					<strong>
+						Sie können der Speicherung widersprechen, sofern Ihre Interessen
+						unser berechtigtes Interesse überwiegen.
+					</strong>
 				</p>
 				<p>
-					<strong>Abschluss eines Data-Processing-Agreements</strong>
+					<strong>Abschluss eines Vertrags über Auftragsverarbeitung</strong>
 				</p>
 				<p>
-					MailChimp hat ein sog. Data-Processing-Agreement in seine AGBs
-					aufgenommen, in dem sich MailChimp verpflichtet, die Daten unserer
-					Kunden zu schützen und sie nicht an Dritte weiterzugeben.
-				</p>
-				<p>
-					Das Data-Processing-Agreement kann online eingesehen werden:
-					<a
-						href="https://mailchimp.com/legal/data-processing-addendum/"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="underline"
-					>
-						https://mailchimp.com/legal/data-processing-addendum/
-					</a>
+					Wir haben mit MailJet einen Vertrag zur Auftragsverarbeitung
+					abgeschlossen und setzen die strengen Vorgaben der deutschen
+					Datenschutzbehörden bei der Nutzung von MailJet vollständig um.
 				</p>
 				<h2>5. Plugins und Tools</h2>
 				<h3>Google Web Fonts</h3>
