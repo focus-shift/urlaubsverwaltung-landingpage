ea6ed6982663eb3d39d31977904d6cf1ad9e2976
Benjamin Seber <seber@synyx.de>
2019-07-14 22:21:30 +0200

    first responsive navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,48 +8,52 @@ import "./header.tailwind.css";
 
 export default function Header() {
 	return (
-		<header className="bg-blue-900 flex flex-row items-center justify-end h-10 p-2">
-			<Link
-				href="/"
-				className="flex flex-row items-center"
-				aria-label="Urlaubsverwaltung Startseite"
-			>
-				<BearIcon id="header" width={32} height={32} ariaHidden />
-				<span className="text-white pl-2">Urlaubsverwaltung</span>
-			</Link>
-			<nav className="ml-auto">
-				<Navigation />
+		<header className="bg-blue-900">
+			<nav className="flex flex-wrap p-2 items-center">
+				<Link
+					href="/"
+					className="flex flex-row items-center"
+					aria-label="Urlaubsverwaltung Startseite"
+				>
+					<BearIcon id="header" width={32} height={32} ariaHidden />
+					<span className="text-white pl-2 hidden sm:block">
+						Urlaubsverwaltung
+					</span>
+				</Link>
+				<input type="checkbox" id="mobile-menu-toggle" className="hidden" />
+				<label
+					htmlFor="mobile-menu-toggle"
+					className="ml-auto sm:hidden text-white focus:shadow-outline"
+					tabIndex={0}
+				>
+					Menü
+				</label>
+				<ul className="navigation-list">
+					<li className="navigation-list-item">
+						<Link href="/demo" className="text-white">
+							Live Demo
+						</Link>
+					</li>
+					<li className="navigation-list-item navigation-list-item-social-media">
+						<a
+							href="https://github.com/synyx/urlaubsverwaltung"
+							aria-label="Github Projekt"
+							className="text-blue inline-block"
+						>
+							<GithubIcon role="img" title="Unser Projekt auf Github" />
+						</a>
+					</li>
+					<li className="navigation-list-item navigation-list-item-social-media">
+						<a
+							href="https://twitter.com/synyx_ka"
+							aria-label="Twitter Profil"
+							className="text-blue inline-block"
+						>
+							<TwitterIcon role="img" title="Folge uns auf Twitter" />
+						</a>
+					</li>
+				</ul>
 			</nav>
 		</header>
-	);
-}
-
-function Navigation() {
-	return (
-		<ul className="flex flex-row items-center justify-center">
-			<li className="pr-2">
-				<Link href="/demo" className="text-white">
-					Demo
-				</Link>
-			</li>
-			<li className="p-1">
-				<a
-					href="https://github.com/synyx/urlaubsverwaltung"
-					aria-label="Github Projekt"
-					className="text-blue block"
-				>
-					<GithubIcon role="img" title="Unser Projekt auf Github" />
-				</a>
-			</li>
-			<li className="p-1">
-				<a
-					href="https://twitter.com/synyx_ka"
-					aria-label="Twitter Profil"
-					className="text-blue block"
-				>
-					<TwitterIcon role="img" title="Folge uns auf Twitter" />
-				</a>
-			</li>
-		</ul>
 	);
 }
