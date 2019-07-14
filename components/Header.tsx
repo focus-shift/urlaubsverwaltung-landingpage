be2aca08e8760424f53888413c5ae7c3f2415154
Benjamin Seber <seber@synyx.de>
2019-07-14 20:30:41 +0200

    use aria-label instead of visually hidden text

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,18 @@ import Link from "./Link";
 import BearIcon from "./BearIcon";
 import GithubIcon from "./GithubIcon";
 import TwitterIcon from "./TwitterIcon";
+import "./header.tailwind.css";
 
 export default function Header() {
 	return (
 		<header className="bg-blue-900 flex flex-row items-center justify-end h-10 p-2">
-			<Link href="/" className="flex flex-row items-center">
+			<Link
+				href="/"
+				className="flex flex-row items-center"
+				aria-label="Urlaubsverwaltung Startseite"
+			>
 				<BearIcon id="header" width={32} height={32} ariaHidden />
 				<span className="text-white pl-2">Urlaubsverwaltung</span>
-				<span className="visually-hidden">&nbsp;Startseite</span>
 			</Link>
 			<nav className="ml-auto">
 				<Navigation />
