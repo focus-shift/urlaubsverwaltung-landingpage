25c47d6da3044e2540ad5f5368645f6d760619c8
Benjamin Seber <seber@synyx.de>
2019-11-15 10:42:51 +0100

    use correct component name

--- pages/datenschutz-beta.tsx
+++ pages/datenschutz-beta.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Agb from "../md/datenschutz-beta.mdx";
+import Datenschutz from "../md/datenschutz-beta.mdx";
 import Page from "../components/Page";
 import "./datenschutz.tailwind.css";
 
 export default () => (
 	<Page title={"Datenschutzerklärung der Beta-Phase"}>
 		<div className="container justify-center xl:max-w-6xl mx-auto px-4 mb-12 text-black datenschutz-container">
-			<Agb />
+			<Datenschutz />
 		</div>
 	</Page>
 );
