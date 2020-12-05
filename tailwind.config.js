eef5f79f475d8aad05db90e35e6d1440d51dfbd6
Benjamin Seber <benjamin.seber@focus-shift.de>
2020-12-05 10:47:01 +0100

    move stuff from css to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
 				"1/2": "50%",
 				full: "100%",
 			}),
+			maxWidth: {
+				"7xl": "80rem",
+				"8xl": "88rem",
+				"9xl": "96rem",
+				"10xl": "104rem",
+			},
 		},
 	},
 };
