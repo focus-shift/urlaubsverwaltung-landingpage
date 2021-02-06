b42199812a1ded8b49a3ee9c37b3641db5ee388f
Benjamin Seber <benjamin.seber@focus-shift.de>
2021-02-06 14:02:45 +0100

    add feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,31 @@
 const formatDate = require("date-fns/format");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 const paths = {
 	input: "src",
 	output: "build",
 };
 
+const isProd = process.env.NODE_ENV === "production";
+
+const not = fn => (...args) => !fn(...args);
+const isDraft = post => Boolean(post.data.draft);
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.setTemplateFormats(["njk", "hbs", "md", "txt"]);
 
+	eleventyConfig.addPlugin(pluginRss);
+
 	eleventyConfig.addHandlebarsHelper("date", function (date, format) {
 		return formatDate(date, format);
+	});
+
+	eleventyConfig.addCollection("blogposts", function (collection) {
+		const allBlogPosts = collection
+			.getFilteredByGlob("./src/blog/**/*.md")
+			.reverse();
+
+		return isProd ? allBlogPosts.filter(not(isDraft)) : allBlogPosts;
 	});
 
 	return {
