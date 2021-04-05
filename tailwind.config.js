db403c27fab6947c990af3e856dfbe14982d968a
Benjamin Seber <benjamin.seber@focus-shift.de>
2021-04-05 14:51:56 +0200

    fix purged css classnames used in js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
 		// eleventy generates html files into the `build` directory
 		// @jadex/snowpack-plugin-tailwindcss-jit marks files as changed for snowpack, matching this `purge.content` path.
 		// snowpack pushes these new changes to the browser executing postcss/tailwind on demand.
-		content: ["./build/**/*.{html,js}"],
+		content: ["./build/**/*.{html,js}", "./static/**/*.js"],
 	},
 	theme: {
 		extend: {
