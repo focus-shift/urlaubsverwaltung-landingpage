21142936b945c2d9c52449aa7d7eb7c97f11135b
Benjamin Seber <benjamin.seber@focus-shift.de>
2021-09-21 23:28:01 +0200

    find link when inner span has been clicked

--- static/js/goals.js
+++ static/js/goals.js
@@ -1,17 +1,16 @@
 window.addEventListener("click", function (event) {
 	if (typeof plausible === "function") {
-		const { goal, goalTrigger } = event.target.dataset;
+		const link =
+			event.target.tagName === "A" ? event.target : event.target.closest("a");
+		const { goal, goalTrigger } = link.dataset;
 		if (goal) {
 			// eslint-disable-next-line no-undef
 			plausible(goal, { props: { trigger: goalTrigger } });
-			if (event.target.tagName === "A") {
+			if (!(event.ctrlKey || event.metaKey || event.shiftKey)) {
+				setTimeout(function () {
+					location.href = link.href;
+				}, 100);
 				event.preventDefault();
-				if (!(event.ctrlKey || event.metaKey || event.shiftKey)) {
-					setTimeout(function () {
-						location.href = event.target.href;
-					}, 100);
-					event.preventDefault();
-				}
 			}
 		}
 	}
