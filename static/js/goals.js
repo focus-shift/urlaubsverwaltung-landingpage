29b9fb1985b00b1d030f5299ec1473c0d3cc88f5
Benjamin Seber <benjamin.seber@focus-shift.de>
2021-09-21 23:18:23 +0200

    delay clicked link action to ensure analytics
    plausible defines 150ms in the examples.
    seems to be too much imo, therefore the random 100ms delay :shrug:

--- static/js/goals.js
+++ static/js/goals.js
@@ -4,6 +4,15 @@ window.addEventListener("click", function (event) {
 		if (goal) {
 			// eslint-disable-next-line no-undef
 			plausible(goal, { props: { trigger: goalTrigger } });
+			if (event.target.tagName === "A") {
+				event.preventDefault();
+				if (!(event.ctrlKey || event.metaKey || event.shiftKey)) {
+					setTimeout(function () {
+						location.href = event.target.href;
+					}, 100);
+					event.preventDefault();
+				}
+			}
 		}
 	}
 });
