3a35d70669197adc296aa2b11b57eb9af30287c6
Benjamin Seber <benjamin.seber@focus-shift.de>
2021-10-28 00:32:57 +0200

    intercept links with hashtag and scroll smoothly into view

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -12,6 +12,23 @@ window.addEventListener(
 	{ passive: true },
 );
 
+window.addEventListener("click", function (event) {
+	const link =
+		event.target.tagName === "A" ? event.target : event.target.closest("a");
+	if (!link) {
+		return;
+	}
+	const { hash } = link;
+	if (hash) {
+		const targetElement = document.getElementById(hash.substr(1));
+		if (targetElement) {
+			targetElement.scrollIntoView({ behavior: "smooth" });
+			window.history.pushState(null, null, hash);
+			event.preventDefault();
+		}
+	}
+});
+
 function updateBodyScrollLock() {
 	if (!mdScreen() && menu.checked) {
 		document.body.classList.add("overflow-hidden");
