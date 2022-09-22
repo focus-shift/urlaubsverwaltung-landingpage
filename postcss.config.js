f357248a4ef7108bb432b8e2f59ac1ab34b532f0
Benjamin Seber <benjamin.seber@focus-shift.de>
2022-09-22 13:28:33 +0200

    minify css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
+const prod = process.env.NODE_ENV === "production";
+
 module.exports = {
 	plugins: [
 		require("tailwindcss/nesting"),
 		require("tailwindcss"),
 		require("autoprefixer"),
-	],
+		prod && require("cssnano"),
+	].filter(Boolean),
 };
